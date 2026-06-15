796fff8be4b4fa9e825a053b10472b9c533cca3b
KumarRahulDas <119911113+KumarRahulDas@users.noreply.github.com>
2026-06-15 10:41:22 +0530

    Refactor docker_push to use password-stdin for login

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,17 @@
-def call(String Project, String ImageTag, String dockerhubuser){
-  withCredentials([usernamePassword(credentialsId: 'dockerhubcred', passwordVariable: 'dockerhubpass', usernameVariable: 'dockerhubuser')]) {
-      sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
-  }
-  sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
+def call(String Project, String ImageTag, String DockerHubUser) {
+
+    withCredentials([
+        usernamePassword(
+            credentialsId: 'dockerhubcred',
+            usernameVariable: 'DOCKER_USER',
+            passwordVariable: 'DOCKER_PASS'
+        )
+    ]) {
+
+        sh """
+        echo "\$DOCKER_PASS" | docker login -u "\$DOCKER_USER" --password-stdin
+        docker push ${DockerHubUser}/${Project}:${ImageTag}
+        docker logout
+        """
+    }
 }
